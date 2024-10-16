c09c9a787f8645e7b08a20a08970df9853e6cbbb
Cesar <cesarnsingi@hotmail.com>
2024-10-16 11:56:42 +0400

    responsive button

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ let password = document.getElementById("password");
 eyeicon.onclick = function(){
     if(password.type == "password"){
         password.type = "text";
+        eyeicon.src = "eye-open.png";
     }else{
         password.type = "password";
+        eyeicon.src = "eye-close.png";
+
     }
 }
